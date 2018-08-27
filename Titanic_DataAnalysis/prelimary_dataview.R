6c3b8bf9de507532e40c4235c9d27660014a3e47
lfga98 <lfgarciauaz@gmail.com>
2018-08-26 19:02:36 -0500

    First preliminary Titanic data

---
+++ Titanic_DataAnalysis/prelimary_dataview.R
@@ -0,0 +1,39 @@
+setwd("~/Documents/Ingenieria de Software/Ciencia de Datos/Titanic_DataAnalysis")
+titanic_data <- read.csv("titanic.csv",header = TRUE)
+
+head(titanic_data)
+tail(titanic_data)
+nrow(titanic_data)
+
+
+summary(titanic_data$Survived)
+summary(titanic_data$Pclass)
+summary(titanic_data$Sex)
+summary(titanic_data$Age)
+summary(titanic_data)
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+boxplot(titanic_data$Pclass)
+getmode(titanic_data$Pclass)
+hist(titanic_data$Pclass)
+
+
+boxplot(titanic_data$Survived)
+getmode(titanic_data$Survived)
+hist(titanic_data$Survived)
+boxplot(titanic_data$Age)
+hist(titanic_data$Age)
+boxplot(titanic_data$SibSp)
+getmode(titanic_data$SibSp)
+hist(titanic_data$SibSp)
+boxplot(titanic_data$Parch)
+getmode(titanic_data$Parch)
+hist(titanic_data$Parch)
+getmode(titanic_data$Embarked)
+
+plot(titanic_data$Pclass,titanic_data$Fare)
+
