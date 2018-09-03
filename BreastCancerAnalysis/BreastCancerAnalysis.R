1a8132f25e9856299fe3837ee9456e9656d72966
lfga98 <lfgarciauaz@gmail.com>
2018-09-03 13:25:48 -0500

    Getting first data

--- BreastCancerAnalysis/BreastCancerAnalysis.R
+++ BreastCancerAnalysis/BreastCancerAnalysis.R
@@ -1,1 +1,13 @@
 setwd("~/Documents/Ingenieria de Software/Ciencia de Datos/BreastCancerAnalysis")
+wbcd <- read.csv("wisc_bc_data.csv", stringsAsFactors = FALSE)
+str(wbcd)
+#Exclude id
+wbcd <- wbcd[-1] 
+#Selecting the outcome variable
+table(wbcd$diagnosis)
+#Output variable as factor
+wbcd$diagnosis <- factor(wbcd$diagnosis,
+                         levels=c("B","M"),
+                         labels = c(" Benign","Malignant"))
+
+round( prop.table(table(wbcd$diagnosis))*100, digits = 1)
