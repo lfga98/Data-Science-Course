21245ec30c59e948a5f2fcf68b9924168ae4bf71
lfga98 <lfgarciauaz@gmail.com>
2018-09-05 18:09:38 -0500

    Normalized data

--- BreastCancerAnalysis/BreastCancerAnalysis.R
+++ BreastCancerAnalysis/BreastCancerAnalysis.R
@@ -11,3 +11,11 @@ wbcd$diagnosis <- factor(wbcd$diagnosis,
                          labels = c(" Benign","Malignant"))
 
 round( prop.table(table(wbcd$diagnosis))*100, digits = 1)
+summary(wbcd[ c("radius_mean","area_mean","smoothness_mean")])
+
+normalize <-function(x){
+    return((x-min(x))/(max(x) -min(x)))
+}
+
+wbcd_n <- as.data.frame(lapply(wbcd[2:31], normalize))
+summary(wbcd_n$area_mean)
